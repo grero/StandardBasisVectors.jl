e38fd29a72e05713c0a24ae93d5c2f531e42d4cf
roger <roger.herikstad@gmail.com>
2024-03-05 15:20:49 +0800

    Modest speed improvements

--- src/StandardBasisVectors.jl
+++ src/StandardBasisVectors.jl
@@ -20,17 +20,22 @@ function rot!(R::AbstractMatrix{T}, i,j, θ) where T <: Real
 end
 
 function get_rotation_matrix(N::Integer, i::Integer, θ::Vector{T}) where T <:Real
-    R = diagm(fill(one(T),N)) 
+    R = Matrix{T}(I(N)) 
+    Rt = fill!(similar(R), 0.0)
     for j in range(N, stop=i+1, step=-1)
-        R .= rot(N, i,j, θ[j])*R
+        rot!(Rt, i,j, θ[j])
+        R .= Rt*R
     end
     R
 end
 
 function get_rotation_angles(N::Integer,i::Integer,v::AbstractVector{T}) where T <: Real
-    θ = zeros(T,N+1)
+    θ = zeros(T,N)
     r = one(T)
-    for j in range(N, stop=i+1, step=-1)
+
+    # do first step first
+    θ[N] = asin(v[N])
+    for j in range(N-1, stop=i+1, step=-1)
         y = v[j]
         r = r*cos(θ[j+1])
         if r != 0
@@ -39,32 +44,37 @@ function get_rotation_angles(N::Integer,i::Integer,v::AbstractVector{T}) where T
             θ[j] = 0.0
         end
     end
-    θ[1:N]
+    θ
 end
 
 function reduce_dimensions(N::Integer, i::Integer, V::AbstractMatrix{T}) where T <: Real
     if i == N
         return V, zeros(T, N)
     end
-    v = V[:,i]
+    v = view(V,:,i)
     θ = get_rotation_angles(N,i,v)
     R = get_rotation_matrix(N, i, θ)
     return R'*V, θ
 end
 
 function orient_vectors(V::AbstractMatrix{T}) where T <: Real
+    size(V,1) == size(V,2) || error("Matrix should be square")
     _V = similar(V)
     _V .= V
     N = size(V,1)
     signflip = zeros(T, N)
     θ = zeros(T,size(V)...) 
     for i in 1:N
-        if _V[i,i] >= 0.0
+        @inbounds if _V[i,i] >= 0.0
             signflip[i] = 1.0
         else
             signflip[i] = -1.0
         end
-        _V[:,i] .= _V[:,i].*signflip[i]
+        si = signflip[i]
+        for j in 1:N 
+            @inbounds _V[j,i] *= si
+        end
+
         _V, θ[:,i] = reduce_dimensions(N,i,_V)
     end
     return V*diagm(signflip), signflip,θ 
